c4d9d9095b54f63d1f3704b32d0462886061b727
RushikeshGorde <134618502+RushikeshGorde@users.noreply.github.com>
2024-03-08 20:38:30 +0530

    curser change

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@ const navMenu = document.getElementById('sidebar'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 // sider shoow 
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
 // validate if contact exists 
 if(navToggle) {
     navToggle.addEventListener("click", () => {
